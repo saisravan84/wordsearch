28da2b29b7773ac59c4e695553d331bef964751f
saisravan84 <saisravan84@gmail.com>
2021-07-04 15:22:43 +0530

    Wordsearch-api: added code to overlap

--- src/main/java/com/sravan/wordsearchapi/services/WordGridService.java
+++ src/main/java/com/sravan/wordsearchapi/services/WordGridService.java
@@ -45,8 +45,8 @@ public class WordGridService {
             }
         }
 
-        Collections.shuffle(coordinates);
         for(String word : words) {
+            Collections.shuffle(coordinates);
             for(Coordinate coordinate: coordinates){
                 int x = coordinate.x;
                 int y = coordinate.y;
@@ -111,37 +111,43 @@ public class WordGridService {
             case VERTICAL:
                 if(coordinate.x + wordLength > gridSize) return false;
                 for (int i = 0; i < wordLength; i++) {
-                    if(contents[coordinate.x + i][coordinate.y] != '_') return false;
+                    char letter = contents[coordinate.x + i][coordinate.y];
+                    if(letter != '_' && letter != word.charAt(i)) return false;
                 }
                 break;
             case HORIZANTAL:
                 if(coordinate.y + wordLength > gridSize) return false;
                 for (int i = 0; i < wordLength; i++) {
-                    if(contents[coordinate.x][coordinate.y + i] != '_') return false;
+                    char letter = contents[coordinate.x][coordinate.y + i];
+                    if(letter != '_' && letter != word.charAt(i)) return false;
                 }
                 break;
             case DIAGONAL:
                 if(coordinate.y + wordLength > gridSize || coordinate.x + wordLength > gridSize) return false;
                 for (int i = 0; i < wordLength; i++) {
-                    if(contents[coordinate.x + i][coordinate.y + i] != '_') return false;
+                    char letter = contents[coordinate.x + i][coordinate.y + i];
+                    if(letter != '_' && letter != word.charAt(i)) return false;
                 }
                 break;
             case VERTICAL_INVERSE:
                 if(coordinate.x - wordLength < 0) return false;
                 for (int i = 0; i < wordLength; i++) {
-                    if(contents[coordinate.x - i][coordinate.y] != '_') return false;
+                    char letter = contents[coordinate.x - i][coordinate.y];
+                    if(letter != '_' && letter != word.charAt(i)) return false;
                 }
                 break;
             case HORIZANTAL_INVERSE:
                 if(coordinate.y - wordLength < 0) return false;
                 for (int i = 0; i < wordLength; i++) {
-                    if(contents[coordinate.x][coordinate.y - i] != '_') return false;
+                    char letter = contents[coordinate.x][coordinate.y - i];
+                    if(letter != '_' && letter != word.charAt(i)) return false;
                 }
                 break;
             case DIAGONAL_INVERSE:
                 if(coordinate.y - wordLength < 0 || coordinate.x - wordLength < 0) return false;
                 for (int i = 0; i < wordLength; i++) {
-                    if(contents[coordinate.x - i][coordinate.y - i] != '_') return false;
+                    char letter = contents[coordinate.x - i][coordinate.y - i];
+                    if(letter != '_' && letter != word.charAt(i)) return false;
                 }
                 break;
         }
